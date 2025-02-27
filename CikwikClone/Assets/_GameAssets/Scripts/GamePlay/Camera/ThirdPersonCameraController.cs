f2b9bcac4c1c730f4d10ae539361220988e652d8
ismailusta <ismail.usta028@gmail.com>
2025-02-27 23:48:42 +0300

    v0.4 Camera settings is done

---
+++ CikwikClone/Assets/_GameAssets/Scripts/GamePlay/Camera/ThirdPersonCameraController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private Transform _playerVisualTransform;
+    [SerializeField] private Transform _orientationTransform;
+
+    [Header("Settings")]
+    [SerializeField] private float _rotationSpeed;
+
+    private void Update()
+    {
+        Vector3 viewDirection = _playerTransform.position - new Vector3(transform.position.x, _playerTransform.position.y, transform.position.z);
+        _orientationTransform.forward = viewDirection.normalized;
+
+        float inputHorizontal = Input.GetAxisRaw("Horizontal");
+        float inputVertical = Input.GetAxisRaw("Vertical");
+        Vector3 inputDirection = _orientationTransform.forward * inputVertical + _orientationTransform.right * inputHorizontal;
+        if (inputDirection != Vector3.zero)
+        {
+            _playerVisualTransform.forward = Vector3.Slerp(_playerVisualTransform.forward, inputDirection.normalized, Time.deltaTime * _rotationSpeed);
+        }
+
+    }
+
+}
